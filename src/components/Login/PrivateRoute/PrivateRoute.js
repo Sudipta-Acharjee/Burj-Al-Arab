e6a9d3202db3e083ff2a6fbc5ec9be6429bb86b2
Sudipta Acharjee <sudiptoacharjee4@gmail.com>
2021-03-18 13:44:34 +0600

    first commit

---
+++ src/components/Login/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useContext } from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { UserContext } from '../../../App';
+
+const PrivateRoute = ({ children, ...rest }) => {
+    const [logInUser, setLogInUser]= useContext(UserContext)
+    return (
+        <Route
+            {...rest}
+            render={({ location }) =>
+                logInUser.email ? (
+                    children
+                ) : (
+                        <Redirect
+                            to={{
+                                pathname: "/login",
+                                state: { from: location }
+                            }}
+                        />
+                    )
+            }
+        />
+    );
+};
+
+export default PrivateRoute;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAktVFrIeLAW41TI6MqlRfsqugLnH1-ffQ",
+    authDomain: "burj-al-arab-2ada1.firebaseapp.com",
+    projectId: "burj-al-arab-2ada1",
+    storageBucket: "burj-al-arab-2ada1.appspot.com",
+    messagingSenderId: "900390291556",
+    appId: "1:900390291556:web:1163696eaf72d50a85f771"
+  };
+  export default firebaseConfig;
